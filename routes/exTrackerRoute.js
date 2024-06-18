2cad5ddc7e64c2c8a52b5ba0c951c7918b842858
HighSoft <highsoft85@outlook.com>
2024-06-18 15:50:03 +0200

    Fixed Mongodb

--- routes/exTrackerRoute.js
+++ routes/exTrackerRoute.js
@@ -107,7 +107,6 @@ router.post('/users/:_id/exercises', async (req, res) => {
     
     // Save the exercise
     const exercise = await newExercise.save();
-    console.log(exercise);
     const exerciseObject = {
       "_id": userID,
       "username": exercise.username,
@@ -124,8 +123,8 @@ router.post('/users/:_id/exercises', async (req, res) => {
 });
 
 // PATH /api/users/:_id/logs?[from][&to][&limit]
-router.get('/api/users/:_id/logs', (req, res) => {
-  const id = req.body["_id"] || req.params._id;
+router.get('/users/:_id/logs', async (req, res) => {
+  const id = req.body[":_id"] || req.params._id;
   var fromDate = req.query.from;
   var toDate = req.query.to;
   var limit = req.query.limit;
@@ -158,98 +157,75 @@ router.get('/api/users/:_id/logs', (req, res) => {
   }
 
   // Get the user's information
-  User.findOne({ "_id" : id }, (error, data) => {
-    if (error) {
+  try {
+    const user = await User.findOne({"_id": id});
+    if (!user) {
       res.json("Invalid UserID");
-      return console.log(error);
+      return;
     }
-    if (!data) {
-      res.json("Invalid UserID");
-    } else {
 
-      // Initialize the object to be returned
-      const usernameFound = data.username;
-      var objToReturn = { "_id" : id, "username" : usernameFound };
+    // Initialize the object to be returned
+    const userName = user.username;
+    var data = {"_id": id, "username": userName};  
 
-      // Initialize filters for the count() and find() methods
-      var findFilter = { "username" : usernameFound };
-      var dateFilter = {};
+    // Initialize filters for the count() and find() methods
+    var findFilter = {"username": userName};
+    var dateFilter = {};
 
-      // Add to and from keys to the object if available
-      // Add date limits to the date filter to be used in the find() method on the Exercise model
-      if (fromDate) {
-        objToReturn["from"] = fromDate.toDateString();
-        dateFilter["$gte"] = fromDate;
-        if (toDate) {
-          objToReturn["to"] = toDate.toDateString();
-          dateFilter["$lt"] = toDate;
-        } else {
-          dateFilter["$lt"] = Date.now();
-        }
-      }
-
+    // Add to and from keys to the object if available
+    // Add date limits to the date filter to be used in the find() method on the Exercise model
+    if (fromDate) {
+      data["from"] = fromDate.toDateString();
+      dateFilter["$gte"] = fromDate;
       if (toDate) {
-        objToReturn["to"] = toDate.toDateString();
+        data["to"] = toDate.toDateString();
         dateFilter["$lt"] = toDate;
-        dateFilter["$gte"] = new Date("1960-01-01");
+      } else {
+        dateFilter["$lt"] = Date.now();
       }
-
-      // Add dateFilter to findFilter if either date is provided
-      if (toDate || fromDate) {
-        findFilter.date = dateFilter;
-      }
-
-      // console.log(findFilter);
-      // console.log(dateFilter);
-
-      // Add the count entered or find the count between dates
-      Exercise.count(findFilter, (error, data) => {
-        if (error) {
-          res.json("Invalid Date Entered");
-          return console.log(error);
-        }
-        // Add the count key 
-        var count = data;
-        if (limit && limit < count) {
-          count = limit;
-        }
-        objToReturn["count"] = count;
-
-
-        // Find the exercises and add a log key linked to an array of exercises
-        Exercise.find(findFilter, (error, data) => {
-          if (error) return console.log(error);
-
-          // console.log(data);
-
-          var logArray = [];
-          var objectSubset = {};
-          var count = 0;
-
-          // Iterate through data array for description, duration, and date keys
-          data.forEach(function(val) {
-            count += 1;
-            if (!limit || count <= limit) {
-              objectSubset = {};
-              objectSubset.description = val.description;
-              objectSubset.duration = val.duration;
-              objectSubset.date = val.date.toDateString();
-              console.log(objectSubset);
-              logArray.push(objectSubset);
-            }
-          });
-
-          // Add the log array of objects to the object to return
-          objToReturn["log"] = logArray;
-
-          // Return the completed JSON object
-          res.json(objToReturn);
-        });
-
-      });
-
     }
-  });
+
+    if (toDate) {
+      data["to"] = toDate.toDateString();
+      dateFilter["$lt"] = toDate;
+      dateFilter["$gte"] = new Date("1960-01-01");
+    }
+
+    // Add dateFilter to findFilter if either date is provided
+    if (toDate || fromDate) {
+      findFilter.date = dateFilter;
+    }
+
+    // Add the count entered or find the count between dates
+    let count = await Exercise.countDocuments(findFilter); 
+    if (limit && limit < count) {
+      count = limit;
+    }
+    // Add the count key 
+    data["count"] = count;
+
+    // Find the exercises and add a log key linked to an array of exercises
+    let logs = [];
+    let temp = {};
+    count = 0;
+    const exercises = await Exercise.find(findFilter);
+    exercises.forEach(function(exercise) {
+      count += 1;
+      if (!limit || count <= limit) {
+        temp = {};
+        temp.description = exercise.description;
+        temp.duration = exercise.duration;
+        temp.date = exercise.date.toDateString();
+        logs.push(temp);
+      }
+    });
+
+    data["log"] = logs;
+    res.json(data);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // ----------------
@@ -257,11 +233,13 @@ router.get('/api/users/:_id/logs', (req, res) => {
 
 // PATH /api/exercises/
 // Display all of the exercises in the Mongo DB model titled Exercise
-router.get('/api/exercises', (req, res) => {
-  Exercise.find({}, (error, data) => {
-    if (error) return console.log(error);
+router.get('/api/exercises', async (req, res) => {
+  try {
+    const data = await Exercise.find();
     res.json(data);
-  })
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
